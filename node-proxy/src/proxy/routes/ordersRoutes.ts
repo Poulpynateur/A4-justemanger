8c47853e1e4f1b2caa65da32a15cd2bc797b980b
ndegheselle <nicolas.degheselle@viacesi.fr>
2021-06-26 19:04:16 +0200

    Added order routes

--- node-proxy/src/proxy/routes/ordersRoutes.ts
+++ node-proxy/src/proxy/routes/ordersRoutes.ts
@@ -4,5 +4,38 @@ import config from '../../config/config';
 
 let router = express.Router();
 
+/**
+ * @swagger
+ * /orders:
+ *   get:
+ *     description: Get list of orders. Need role "management".
+ *     tags:
+ *       - Orders
+ *     security:
+ *         - bearerAuth: []
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: List of all the orders.
+ */
+router.get('/orders', httpProxy(config.services.orders + '/orders'));
+
+/**
+ * @swagger
+ * /orders/{id}:
+ *   get:
+ *     description: Get an user info. Need role "management".
+ *     tags:
+ *       - Orders
+ *     security:
+ *         - bearerAuth: []
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Specific order.
+ */
+router.get('/orders/:id', httpProxy(config.services.orders + '/login'));
 
 export default router;
