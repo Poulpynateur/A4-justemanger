79fb725d6285b5c59d520af20c5a8bf9f8dfe150
ndegheselle <nicolas.degheselle@viacesi.fr>
2021-06-29 15:52:32 +0200

    Corrected update and delete user

--- node-service-users/src/API/routes/userRoutes.ts
+++ node-service-users/src/API/routes/userRoutes.ts
@@ -1,13 +1,17 @@
 import * as express from "express";
-import userIF from '../webservices/userWS';
-import authMiddleware from '../middleware/authMiddleware';
+
+import userWS from '../webservices/userWS';
+import auth from '../middleware/authMiddleware';
 
 let router = express.Router();
 
-router.get('/', userIF.listAll);
-router.post('/', userIF.create);
-router.get('/:id', userIF.read);
-router.put('/:id', userIF.update);
-router.delete('/:id', userIF.delete);
+// router.get('/', userIF.listAll);
+// router.post('/', userIF.create);
+// router.get('/:id', userIF.read);
+// router.put('/:id', userIF.update);
+// router.delete('/:id', userIF.delete);
+
+router.put('/:id', auth.connected, userWS.update);
+router.delete('/:id', auth.connected, userWS.remove);
 
 export default router;
